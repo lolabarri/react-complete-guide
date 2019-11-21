6e2ea8950255a1e84561d36ae137200aa2e64d4d
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-11-21 21:09:36 +0100

    passing methods to a component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ class App extends Component {
       { name: "Leti", age: 30 },
       { name: "Vir", age: 28 },
       { name: "Pablo", age: 36 }
-    ]
+    ],
+    otherState: "some other value"
+  };
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 30 },
+        { name: "Vir", age: 28 },
+        { name: "Pablo", age: 34 }
+      ]
+    });
   };
 
   render() {
@@ -16,15 +27,24 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+        <button onClick={() => this.switchNameHandler("Leticia")}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Laetitia")}
+        >
           My hobbies: watching Gran Hermano
         </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
-    // return React.createElement('div', {className:"App"}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
