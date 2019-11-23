e8e8f6a868c09806978d9f905003fad3b47f8bef
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-11-23 13:19:35 +0100

    lists and state

--- src/App.js
+++ src/App.js
@@ -13,16 +13,6 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = newName => {
-    this.setState({
-      persons: [
-        { name: newName, age: 30 },
-        { name: "Vir", age: 28 },
-        { name: "Pablo", age: 34 }
-      ]
-    });
-  };
-
   changeHandler = event => {
     this.setState({
       persons: [
@@ -31,6 +21,12 @@ class App extends Component {
         { name: "Pablo", age: 34 }
       ]
     });
+  };
+
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -54,8 +50,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+              />
+            );
           })}
         </div>
       );
