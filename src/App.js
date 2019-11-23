70090bca5a920ccb86e6e6d0d471a1581c054e7d
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-11-23 13:53:57 +0100

    flexible lists

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ class App extends Component {
     showPersons: false
   };
 
-  changeHandler = event => {
-    this.setState({
-      persons: [
-        { name: "Leti", age: 30 },
-        { name: event.target.value, age: 28 },
-        { name: "Pablo", age: 34 }
-      ]
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+    const person = { ...this.state.persons[personIndex] };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   deletePersonHandler = personIndex => {
@@ -58,6 +62,7 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
+                changed={event => this.nameChangeHandler(event, person.id)}
               />
             );
           })}
