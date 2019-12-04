7013ce05ff2147674e287cfa67511a068a02173c
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-12-04 18:53:34 +0100

    styling components with Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
+import Radium from "radium";
 
 class App extends Component {
   state = {
@@ -48,7 +49,11 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     };
 
     let persons = null;
@@ -70,6 +75,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black"
+      }
     }
 
     const classes = [];
@@ -93,4 +102,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
