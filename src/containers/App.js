d43603207319eea02e95cf5af92fe8bbe294f2f6
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-12-09 14:21:59 +0100

    restructuring project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import classes from "./App.css";
-import Person from "./Person/Person";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
+import Person from "../components/Persons/Person/Person";
 
 class App extends Component {
   state = {
@@ -53,12 +52,12 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}><Person
+              <Person key={person.id}
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
                 changed={event => this.nameChangeHandler(event, person.id)}
-              /></ErrorBoundary>
+              />
             );
           })}
         </div>
