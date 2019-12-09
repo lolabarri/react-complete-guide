6c7206010305bffc6b8b53e91d2ed88a8114379b
Leticia Olábarri <leticiaolabarri@yahoo.es>
2019-12-09 12:18:49 +0100

    using CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -45,6 +45,8 @@ class App extends Component {
 
     let persons = null;
 
+    let btnClass = "";
+
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -61,21 +63,24 @@ class App extends Component {
           })}
         </div>
       );
+
+      btnClass = classes.Red;
+
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold")
+      assignedClasses.push(classes.bold)
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <button className="button" onClick={this.togglePersonsHandler}>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}
